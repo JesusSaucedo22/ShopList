993868a688bcd9d571f31e5338e573cb0c4e6488
Jesus Saucedo <23470133@capeche.tecnm.mx>
2025-11-21 11:07:19 -0600

    feat: implementar página de lista de compras con MVVM

--- ShopList.Gui/Models/Item.cs
+++ ShopList.Gui/Models/Item.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public string Nombre { get; set; } = string.Empty;
         public int Cantidad { get; set; }
+        public bool Comprado { get; set; } = false;
+
 
         public override string ToString()
         {
--- ShopList.Gui/ViewModels/ShopListViewModel.cs
+++ ShopList.Gui/ViewModels/ShopListViewModel.cs
@@ -1,41 +1,82 @@
-﻿using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.ObjectModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using ShopList.Gui.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 
-public class ShopListViewModel
+namespace ShopList.Gui.ViewsModels
 {
-    public ObservableCollection<Item> Items { get; set; }
-
-    public ShopListViewModel()
+    public class ShopListViewModel : ObservableObject
     {
-        Items = new ObservableCollection<Item>();
-        CargarDatos();
-    }
-    private void CargarDatos()
-    {
+        [ObservableProperty]
+        private string _nombreDelArticulo = string.Empty;
+        [ObservableProperty]
+        private int _cantidadACompra = 1;
 
-        Items.Add(new Item()
+        //public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ObservableCollection<Item> Items { get; }
+
+        
+
+        /*public void AgregarShopListItem()
         {
-            Id = 0,
-            Nombre = "Leche",
-            Cantidad = 2,
-        });
-        Items.Add(new Item()
+            if(string.IsNullOrEmpty(_nombreDelArticulo) || CantidadAComprar <= 0)
+           {
+                return;
+            }
+            Random generador = new Random();
+            var item = new Item
+            {
+                Id = generador.Next(),
+                Nombre = NombreDelArticulo,
+               Cantidad = CantidadAComprar,
+                Comprado = false,
+        /*    };
+            Items.Add(item);
+            NombreDelArticulo = string.Empty;
+            CantidadAComprar = 1;
+        }*/
+
+        public void EliminarShopListItem()
         {
-            Id = 0,
-            Nombre = "Leche",
-            Cantidad = 2,
-        });
-        Items.Add(new Item()
+
+        }
+
+        private void CargarDatos()
         {
-            Id = 0,
-            Nombre = "Leche",
-            Cantidad = 2,
-        });
+
+            Items.Add(new Item()
+            {
+                Id = 1,
+                Nombre = "Leche",
+                Cantidad = 2,
+                Comprado = false,
+            });
+            Items.Add(new Item()
+            {
+                Id = 2,
+                Nombre = "Leche",
+                Cantidad = 2,
+                Comprado = false,
+            });
+            Items.Add(new Item()
+            {
+                Id = 3,
+                Nombre = "Leche",
+                Cantidad = 2,
+                Comprado = false,
+            });
+
+        }
+
+        //private void OnPropertyChanged(string propertyName)
+       // {
+       //     PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+       // }
+
 
     }
+
 }
 
